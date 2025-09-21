a020d5dc1300a1c03913e78c18f0af70b20a9afb
rudarmihael10@gmail.com <rudarmihael10@gmail.com>
2025-09-22 01:24:47 +0200

    Dodajem treci domaci

---
+++ domaci3.py
@@ -0,0 +1,10 @@
+#Domaci3
+
+# ako je tajni pin 4231 ILI ako je tajni pin 3124 ispisati poruku "PIN JE TACAN"
+# U suprotnom izbaciti poruku "NETACAN PIN!"
+
+pin = 2341
+if pin == 4231 or pin == 3124:
+    print("Pin je tacan!")
+else:
+    print("Pin je netacan!")
