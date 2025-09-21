6b2f11b3be238c65db567f26229287b924472b2c
rudarmihael10@gmail.com <rudarmihael10@gmail.com>
2025-09-22 00:52:46 +0200

    first commit

---
+++ domaci1.py
@@ -0,0 +1,12 @@
+#Domaci 1
+
+# Kako da promenite pr u stavku iz liste "Harry Potter 1" -> "Pragmatic programmer"
+# Kako da obrisem posljednju stavku iz liste
+
+books = ["Harry Potter 1", "LOTR", "Harry Potter 2"]
+
+books[0] = "Pragmatic programmer"
+
+books.remove("Harry Potter 2")
+
+print(books)
