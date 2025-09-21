1ea0d0137e22ca04f839b953b9e8c770f4fa249c
rudarmihael10@gmail.com <rudarmihael10@gmail.com>
2025-09-22 01:09:22 +0200

    Dodajem drugi zadatak

---
+++ Domaci2.py
@@ -0,0 +1,22 @@
+#napraviti dictionary koji ima sledecu strukturu
+
+# person1: "name": "Toma", "last_name": "Nikolic"
+# person2: "name": "Petar", "last_name": "Pavlovic"
+# person3: "name": "Mihajlo", "last_name": "Folic"
+
+people = {
+    "person1": {
+        "name": "Toma",
+        "last_name": "Nikolic"
+    },
+    "person2": {
+        "name": "Petar",
+        "last_name": "Pavlovic"
+    },
+    "person3": {
+        "name": "Mihajlo",
+        "last_name": "Folic}"
+    }
+}
+
+print(people)
